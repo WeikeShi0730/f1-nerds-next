013eb2aa14e38c3c58a7fe92f60d06d1ca026c9d
Weike Shi <weikeshih@gmail.com>
2022-03-18 22:57:05 -0400

    added 2022 to the list

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const server = dev
   ? "http://127.0.0.1:5000"
   : "https://f1-nerds-flask.herokuapp.com/";
 
-export const years = [2018, 2019, 2020, 2021];
+export const years = [2018, 2019, 2020, 2021, 2022];
 
 export const colors = [
   "#0f172a",
