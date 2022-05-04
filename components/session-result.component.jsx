b7f53f98d74d9506e4a154005a2c421de585f5e0
Weike Shi <weikeshih@gmail.com>
2022-05-03 23:06:29 -0400

    Fix session result bug

--- components/session-result.component.jsx
+++ components/session-result.component.jsx
@@ -1,44 +1,38 @@
 import React, { useState, useEffect } from "react";
+import { server } from "../config";
 
 const SessionResult = ({ gp, year, session }) => {
-  const [sessionResult, setSessionResult] = useState();
+  const [sessionResult, setSessionResult] = useState(null);
   useEffect(() => {
     const getData = async (gp, year, session) => {
       try {
         const res = await fetch(
           `${server}/api/year/${year}/weekend/${gp}/session/${session}`
         );
-        const json = await res.json();
-        console.log(json);
-        // const round = json.round;
-        // const sessions = json.weekend_sessions;
-        // setRound(round);
-        // setSessions(sessions);
         // setSessionsLoading(false);
+        return await res.json();
       } catch (error) {
         console.error(error);
+        return null;
       }
     };
 
     if (
+      year !== undefined &&
+      year !== null &&
       gp !== undefined &&
       gp !== null &&
       session !== undefined &&
       session !== null
     ) {
-      setSessionsLoading(true);
-      // clear other sections //
-      setRound(null);
-      setSession(null);
-      setSelectedDrivers(null);
-      setSessions(null);
-      setDrivers(null);
-      setSessionDataWithId([]);
-      // fetch new data //
-      getData(gp.value, year.value);
+      console.log(gp.value, year.value, session.value);
+      const sessionResult = getData(gp.value, year.value, session.value);
+      setSessionResult(sessionResult);
+    } else {
+      setSessionResult(null);
     }
-  }, [gp, setSelectedDrivers, setSession]);
-  return <div>SessionResult</div>;
+  }, [gp, session, year]);
+  return <>{sessionResult ? "sessionResult" : null}</>;
 };
 
 export default SessionResult;
