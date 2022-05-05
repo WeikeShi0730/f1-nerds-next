cdb40e5f1abb698a0177a5c4f2edcc44a907691f
Weike Shi <weikeshih@gmail.com>
2022-05-04 20:01:08 -0400

    Add loading and fix undefined bug for session result

--- components/session-result.component.jsx
+++ components/session-result.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Spinner from "./spinner.component";
 import { server } from "../config";
 
 const SessionResult = ({ gp, year, session }) => {
@@ -36,17 +37,18 @@ const SessionResult = ({ gp, year, session }) => {
   }, [gp, session, year]);
   return (
     <>
+      <div>{loading ? <Spinner /> : null}</div>
       {sessionResult ? (
         <div className="w-full h-full bg-opacity-40 bg-white px-5 py-10 mx-auto mb-5 rounded-3xl shadow-xl">
           <div className="flex justify-center text-lg">Session Result</div>
           <div className="grid grid-cols-8 justify-items-center">
+            <div>Position</div>
             <div>Driver</div>
+            <div>Grid</div>
+            <div>Points</div>
             <div>Fastest Lap Time</div>
             <div>Fastest Lap</div>
             <div>Fastest Lap Rank</div>
-            <div>Grid</div>
-            <div>Position</div>
-            <div>Points</div>
             <div>Status</div>
           </div>
           {sessionResult.map((eachResult, index) => {
@@ -55,13 +57,13 @@ const SessionResult = ({ gp, year, session }) => {
                 key={index}
                 className="grid grid-cols-8 justify-items-center"
               >
-                <div>{eachResult.Driver.code} </div>
-                <div>{eachResult.FastestLap.Time.time} </div>
-                <div>{eachResult.FastestLap.lap} </div>
-                <div>{eachResult.FastestLap.rank} </div>
-                <div>{eachResult.grid} </div>
                 <div>{eachResult.position} </div>
+                <div>{eachResult.Driver?.code} </div>
+                <div>{eachResult.grid} </div>
                 <div>{eachResult.points} </div>
+                <div>{eachResult.FastestLap?.Time.time} </div>
+                <div>{eachResult.FastestLap?.lap} </div>
+                <div>{eachResult.FastestLap?.rank} </div>
                 <div>{eachResult.status} </div>
               </div>
             );
