66c76d5eb7208b40a606d7de5f45b3e318713df7
Weike Shi <weikeshih@gmail.com>
2022-05-05 23:56:25 -0400

    Fix session result overflow-x-auto

--- components/session-result.component.jsx
+++ components/session-result.component.jsx
@@ -35,41 +35,45 @@ const SessionResult = ({ gp, year, session }) => {
     getData(gp, year, session);
   }, [gp, session, year]);
   return (
-    <>
+    <div className="flex w-full h-full overflow-x-auto">
       <div>{loading ? <Spinner /> : null}</div>
       {sessionResult ? (
-        <div className="w-full h-full px-5 py-10 mx-auto mb-5">
-          <div className="flex justify-center text-lg">Session Result</div>
-          <div className="grid grid-cols-8 justify-items-center">
-            <div>Position</div>
-            <div>Driver</div>
-            <div>Grid</div>
-            <div>Points</div>
-            <div>Fastest Lap Time</div>
-            <div>Fastest Lap</div>
-            <div>Fastest Lap Rank</div>
-            <div>Status</div>
-          </div>
-          {sessionResult.map((eachResult, index) => {
-            return (
-              <div
-                key={index}
-                className="grid grid-cols-8 justify-items-center"
-              >
-                <div>{eachResult.position} </div>
-                <div>{eachResult.Driver?.code} </div>
-                <div>{eachResult.grid} </div>
-                <div>{eachResult.points} </div>
-                <div>{eachResult.FastestLap?.Time.time} </div>
-                <div>{eachResult.FastestLap?.lap} </div>
-                <div>{eachResult.FastestLap?.rank} </div>
-                <div>{eachResult.status} </div>
+        <div className="px-5 py-10 mx-auto">
+          <div className="flex justify-center text-lg mb-8">Session Result</div>
+          <div className="flex ">
+            <div className="shrink-0">
+              <div className="grid grid-cols-8 justify-items-center">
+                <div>Position</div>
+                <div>Driver</div>
+                <div>Grid</div>
+                <div>Points</div>
+                <div>Fastest Lap Time</div>
+                <div>Fastest Lap</div>
+                <div>Fastest Lap Rank</div>
+                <div>Status</div>
               </div>
-            );
-          })}
+              {sessionResult.map((eachResult, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="grid grid-cols-8 justify-items-center"
+                  >
+                    <div>{eachResult.position} </div>
+                    <div>{eachResult.Driver?.code} </div>
+                    <div>{eachResult.grid} </div>
+                    <div>{eachResult.points} </div>
+                    <div>{eachResult.FastestLap?.Time.time} </div>
+                    <div>{eachResult.FastestLap?.lap} </div>
+                    <div>{eachResult.FastestLap?.rank} </div>
+                    <div>{eachResult.status} </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 
