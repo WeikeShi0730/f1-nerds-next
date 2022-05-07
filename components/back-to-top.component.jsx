414471edc481b6d39981134b9cca761c4266eab4
Weike Shi <weikeshih@gmail.com>
2022-05-06 22:16:29 -0400

    Fix back to top transitoin

--- components/back-to-top.component.jsx
+++ components/back-to-top.component.jsx
@@ -3,16 +3,24 @@ import Image from "next/image";
 
 const BackToTop = () => {
   const [scrollPosition, setScrollPosition] = useState(false);
+  let timer = null;
   const handleScroll = () => {
-    const position = window.pageYOffset >= window.innerHeight / 4;
+    const position = window.pageYOffset >= window.innerHeight / 6;
     setScrollPosition(position);
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      setScrollPosition(() => false);
+    }, 1000);
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll, { passive: true });
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleClick = () => {
     window.scroll({
@@ -23,11 +31,11 @@ const BackToTop = () => {
   };
 
   return (
-    <div className="fixed bottom-10 right-10 z-50 ">
+    <div className="fixed bottom-10 right-10 z-50">
       <button onClick={handleClick}>
         <div
-          className={`relative w-10 h-10 ${
-            scrollPosition ? "" : "hidden"
+          className={`relative w-10 h-10 transition-all duration-200 ease-in-out ${
+            scrollPosition || "opacity-0"
           }`}
         >
           <Image
