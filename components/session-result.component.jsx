d72aaf2712cfa2a2b7e61ff8599bcd8460105ed5
Weike Shi <weikeshih@gmail.com>
2023-03-31 22:48:57 -0400

    Update to backend api

--- components/session-result.component.jsx
+++ components/session-result.component.jsx
@@ -44,12 +44,9 @@ const SessionResult = ({ setSessionDataLoading }) => {
               <div className="grid grid-cols-6 justify-items-center">
                 <div>Position</div>
                 <div>Driver</div>
+                <div>Team</div>
                 <div>Grid</div>
                 <div>Points</div>
-                {/* <div>Fastest Lap Time</div>
-                <div>Fastest Lap</div>
-                <div>Fastest Lap Rank</div> */}
-                <div>Time</div>
                 <div>Status</div>
               </div>
               {Object.keys(sessionResult.DriverNumber).map(
@@ -59,15 +56,11 @@ const SessionResult = ({ setSessionDataLoading }) => {
                       key={eachDriver}
                       className="grid grid-cols-6 justify-items-center"
                     >
-                      {/* {console.log(index)} */}
                       <div>{sessionResult.Position[eachDriver]} </div>
                       <div>{sessionResult.BroadcastName[eachDriver]} </div>
+                      <div>{sessionResult.TeamName[eachDriver]} </div>
                       <div>{sessionResult.GridPosition[eachDriver]} </div>
                       <div>{sessionResult.Points[eachDriver]} </div>
-                      {/* <div>{sessionResult.FastestLap?.Time.time} </div>
-                    <div>{sessionResult.FastestLap?.lap} </div>
-                    <div>{sessionResult.FastestLap?.rank} </div> */}
-                      <div>{sessionResult.Time[eachDriver]} </div>
                       <div>{sessionResult.Status[eachDriver]} </div>
                     </div>
                   );
